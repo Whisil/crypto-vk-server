2fe73669e68ce9ad89a4b8afe094edf67f8f22ed
David <electromandarin@gmail.com>
2023-05-01 00:00:33 +0300

    fix: post saving on error fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,20 +16,26 @@ export const createPost = async (req, res) => {
   const { text } = req.body;
   const file = req.file;
 
-  const newPost = new Post({
-    createdBy: new mongoose.Types.ObjectId(req.userId),
-    text,
-    mediaURL: `${req.protocol}://${req.get('host')}/public/media/${file.filename}`,
-  });
-
   try {
-    await newPost.save();
+    const newPost = new Post({
+      createdBy: new mongoose.Types.ObjectId(req.userId),
+      text,
+      mediaURL: `${req.protocol}://${req.get("host")}/public/media/${
+        file.filename
+      }`,
+    });
 
-    await User.findOneAndUpdate(
-      { _id: req.userId },
-      { $push: { posts: newPost._id } },
-      { new: true }
-    );
+    try {
+      await User.findOneAndUpdate(
+        { _id: req.userId },
+        { $push: { posts: newPost._id } },
+        { new: true }
+      );
+    } catch (err) {
+      throw err;
+    }
+
+    await newPost.save();
 
     res.status(201).json(newPost);
   } catch (err) {

f1140af75611453a0cd0ccaa79072957e192f0de
David <electromandarin@gmail.com>
2023-05-20 23:47:14 +0300

    feat: user media posts query

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,7 @@ export const getUserPosts = async (req, res) => {
   const userWallet = req.params.userWallet;
 
   try {
-    const user = await User.findOne({ ethAddress: userWallet }).populate({
+    let query = User.findOne({ ethAddress: userWallet }).populate({
       path: "posts",
       populate: {
         path: "createdBy",
@@ -25,7 +25,20 @@ export const getUserPosts = async (req, res) => {
       },
     });
 
-    res.status(200).json(user.posts);
+    if (req.params.media) {
+      query = query.populate({
+        path: "posts",
+        match: { mediaURL: { $exists: true } },
+        populate: {
+          path: "createdBy",
+          model: "User",
+        },
+      });
+    }
+
+    const populatedUser = await query;
+
+    res.status(200).json(populatedUser.posts);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ import { getUser, getUserPosts } from "../controllers/user.js";
 const router = Router();
 
 router.get("/:userWallet", getUser);
-router.get("/:userWallet/posts", getUserPosts)
+router.get("/:userWallet/posts/:media?", getUserPosts)
 
 export default router;

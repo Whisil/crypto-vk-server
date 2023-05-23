742fd7d6ee0cff431dbcdfc52e454483e3e2a7d1
David <electromandarin@gmail.com>
2023-05-23 22:31:52 +0300

    feat: settings

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,3 @@
-import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
@@ -41,5 +40,61 @@ export const getUserPosts = async (req, res) => {
     res.status(200).json(populatedUser.posts);
   } catch (err) {
     res.status(404).json({ message: err.message });
+  }
+};
+
+export const setSettings = async (req, res) => {
+  const {
+    username,
+    displayName,
+    bannerURL = null,
+    avatarURL = null,
+    bio = null,
+    websiteURL = null,
+  } = req.body;
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (
+      username !== undefined &&
+      username !== user.username &&
+      username.length > 4 &&
+      username.length < 35
+    ) {
+      user.username = username;
+    }
+
+    if (
+      displayName !== undefined &&
+      displayName !== user.displayName &&
+      displayName.length > 4 &&
+      displayName.length < 35
+    ) {
+      user.displayName = displayName;
+    }
+
+    if (bannerURL !== undefined && bannerURL !== user.bannerURL) {
+      user.bannerURL = bannerURL;
+    }
+
+    if (avatarURL !== undefined && avatarURL !== user.avatarURL) {
+      user.avatarURL = avatarURL;
+    }
+
+    if (bio !== undefined && bio !== user.bio) {
+      user.bio = bio;
+    }
+
+    if (websiteURL !== undefined && websiteURL !== user.websiteURL) {
+      user.websiteURL = websiteURL;
+    }
+
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUser, getUserPosts } from "../controllers/user.js";
+import { getUser, getUserPosts, setSettings } from "../controllers/user.js";
 
 const router = Router();
 
 router.get("/:userWallet", getUser);
-router.get("/:userWallet/posts/:media?", getUserPosts)
+router.get("/:userWallet/posts/:media?", getUserPosts);
+router.post("/settings", setSettings);
 
 export default router;

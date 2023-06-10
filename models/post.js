02b4f360393b0dc1de29491fa71cdc6fcd93ebe0
David <electromandarin@gmail.com>
2023-06-10 17:17:46 +0300

    created back Post model for heroku errors

---
+++ models/post.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    text: {
+      required: false,
+      type: String,
+    },
+    createdBy: {
+      required: true,
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    mediaURL: {
+      required: false,
+      type: String,
+    },
+    likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment"}]
+  },
+  { timestamps: true, versionKey: false, collection: "Post" }
+);
+
+postSchema.index({ createAt: 1 });
+
+export default mongoose.model("Post", postSchema);
